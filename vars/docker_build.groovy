311f1d11e5a9692bc6a91968310d36d82c8aaf90
Praveen Kumar <pprasadpraveen03@gmail.com>
2025-09-23 17:54:51 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,4 @@
 def call(String project, String tag , String DocUser){
-  sh "docker build -t "${DocUser}"/"${project}":"${tag}" ."
+  sh "docker build -t ${DocUser}/${project}:${tag} ."
   echo "docker image build sucess!"
+}
